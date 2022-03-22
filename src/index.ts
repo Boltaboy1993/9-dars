d877565b12b475060403ebf0acf308f671bf0896
sokintun7339656 <club_1303@mail.ru>
2022-03-22 19:00:18 +0500

    proaek

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,50 @@
-import prompt_sync from "prompt-sync";
-const prompt = prompt_sync()
+const saveName: HTMLInputElement  = document.querySelector('#save_name')!
+const saveValue: HTMLInputElement = document.querySelector('#save_value')!
+const saveBtn: HTMLButtonElement  = document.querySelector('#save_btn')!
 
-const value = prompt('Enter value:')
+const readName: HTMLInputElement = document.querySelector('#read_name')!
+const data: HTMLElement          = document.querySelector('#data')!
+const readBtn: HTMLButtonElement = document.querySelector('#read_btn')!
+const keys: HTMLUListElement     = document.querySelector('#keys')!
 
-console.log("your value: " + value);
+
+function loadKeys() {
+  for (let index = 0; index < localStorage.length; index++) {
+    let key = localStorage.key(index)!
+    let li = document.createElement('li')
+    li.className = 'list-group-item'
+    li.innerText = key    
+    keys.appendChild(li)
+  }
+}
+
+loadKeys()
+saveBtn.onclick = () => {
+   let abc = saveValue.value.trim()
+   let abd = saveName.value.trim()
+
+   if  (abc === '' || abd === '' ) {
+    alert('bo`sh') 
+    return ''
+   }
+   localStorage.setItem(abd, abc)
+   alert('ma`lumot saqlandi')
+   keys.innerText = ''
+   loadKeys()
+  //  alert(abc+ ' ' +abd)
+}
+
+readBtn.onclick = () => {
+  let name = readName.value.trim()
+  if (name === '') {
+    alert('bo`sh')
+    return
+  }
+  let value = localStorage.getItem(name)
+  if (value === null) {
+    data.innerText = '[ma`lumot]'
+  }
+  else{
+    data.innerText = value
+  }
+}
